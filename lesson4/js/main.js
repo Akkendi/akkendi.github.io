00989c3e81892af585b0f4886bacaef86e2a2162
WDD 230 <akkendiayvaz@gmail.com>
2020-05-23 09:35:41 +0300

    menu bar

--- lesson4/js/main.js
+++ lesson4/js/main.js
@@ -1,4 +1,13 @@
+//this shows the update date in the footer
 let date = new Date(document.lastModified);
 let time = date.toLocaleString('en-US');
     
 document.getElementById("modifiedTime").textContent = time;
+
+//humburger menu
+const hambutton = document.querySelector(".ham");
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+document.querySelector(".navigation").classList.toggle("responsive");
+  }
