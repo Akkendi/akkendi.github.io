6b3e62149fe64733b4932bde776c294aeb207563
WDD 230 <akkendiayvaz@gmail.com>
2020-05-23 23:55:25 +0300

    lesson4 done

--- lesson4/js/main.js
+++ lesson4/js/main.js
@@ -1,13 +1,42 @@
-//this shows the update date in the footer
-let date = new Date(document.lastModified);
-let time = date.toLocaleString('en-US');
-    
-document.getElementById("modifiedTime").textContent = time;
-
 //humburger menu
 const hambutton = document.querySelector(".ham");
 hambutton.addEventListener("click", toggleMenu, false);
 
 function toggleMenu() {
-document.querySelector(".navigation").classList.toggle("responsive");
+document.querySelector("nav ul").classList.toggle("responsive");
   }
+
+//this shows the update date in the footer
+let daynames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+let d = new Date();
+let dayname = daynames[d.getDay()];
+let monthname = months[d.getMonth()];
+let year = d.getFullYear();
+let fulldate = dayname + ", " + d.getDate() + " " + monthname + " " + year;
+
+document.getElementById("modifiedTime").textContent = fulldate;
+console.log(fulldate);
+
+document.getElementById("year").textContent = year;
