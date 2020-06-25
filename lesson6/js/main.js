20205d9160b9a4be7389a090ce7066609dab7191
WDD 230 <akkendiayvaz@gmail.com>
2020-06-25 08:58:13 +0300

    6th lesson done

---
+++ lesson6/js/main.js
@@ -0,0 +1,53 @@
+//humburger menu
+const hambutton = document.querySelector(".ham");
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+document.querySelector("nav ul").classList.toggle("responsive");
+  }
+
+//this shows the update date in the footer
+let daynames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+let d = new Date();
+let dayname = daynames[d.getDay()];
+let monthname = months[d.getMonth()];
+let year = d.getFullYear();
+let fulldate = dayname + ", " + d.getDate() + " " + monthname + " " + year;
+
+document.getElementById("modifiedTime").textContent = fulldate;
+console.log(fulldate);
+
+let b = new Date();
+const banner = document.getElementById("banner");
+
+if(b.getDay() == 5) {
+  banner.style.display = "block";
+}
+else {
+  banner.style.display = "none";
+}
+
+
