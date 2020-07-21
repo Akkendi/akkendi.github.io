7856151df5b69a92da97de0a545991b60f18a2cc
WDD 230 <akkendiayvaz@gmail.com>
2020-07-21 23:52:27 +0300

    lesson 11

---
+++ lesson11/js/weatherapi.js
@@ -0,0 +1,13 @@
+const apiURL = 'api.openweathermap.org/data/2.5/weather?id=4164138&appid=dc3ca0b1e530db7c8b79a01f0377b48e';
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        document.getElementById('current-temp').textContent = jsObject.main.temp;
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
+        const desc = jsObject.weather[0].description;
+        document.getElementById('imagesrc').textContent = imagesrc;
+        document.getElementById('icon').setAttribute('src', imagesrc);
+        document.getElementById('icon').setAttribute('alt', desc);
+    });
+© 2020 GitHub, Inc.
