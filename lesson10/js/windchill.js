6e8d2758c3cdf2701e52d1a51c0700a9a62f4293
WDD 230 <akkendiayvaz@gmail.com>
2020-07-21 23:29:05 +0300

    weather api

--- lesson10/js/windchill.js
+++ lesson10/js/windchill.js
@@ -1,14 +1,19 @@
-function chill(){
-    let temp = parseFloat(document.getElementById("temp").textContent);
-    let wind = parseFloat(document.getElementById("wind").textContent); 
-    
-    if (temp <= 50 && wind > 3){
-    let f = 35.74 + 0.6215 * temp - 35.75 * Math.pow(wind,0.16) + 0.4275 * temp * Math.pow(wind,0.16);
-   
-     document.getElementById("chill").innerHTML = parseInt(f);      
-    } 
-    else{
-     document.getElementById("chill").innerHTML = "N/A";       
-    }
+function getWindChill(){
+  let t = parseFloat(document.getElementById("currentTemp").textContent);
+  let s = parseFloat(document.getElementById("windSpeed").textContent);
+  let x = windChill(t, s);
+
+  document.getElementById("windChill").innerHTML = x;
+}
+
+function windChill(tempF, speed){
+  if (tempF < 50 && speed > 4.8) {
+
+  let output = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * tempF * Math.pow(speed, 0.16));
+
+  return Math.round(output);
   }
-   chill()
+  else {
+      return "N/A";
+  }
+}
